a84dbf29e9c01543d7b7bf8b2d925ca76c022c19
Swornim-Stha <swornimshrestha457@gmail.com>
2025-11-10 07:34:44 +0545

    added issues to route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\IssueController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,6 +24,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Admin routes - only admins can access departments
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('departments', DepartmentController::class);
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Issues routes - all authenticated users can access
+    Route::resource('issues', IssueController::class);
 });
 
 // Admin routes - only admins can access departments
